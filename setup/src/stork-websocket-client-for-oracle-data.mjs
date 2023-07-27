add0af854fe42d49e559084796e0d609edf688a6
Theodore Chaikalis <teo@mystenlabs.com>
2023-07-27 16:50:25 +0300

    add websocket client for Stork Oracle

---
+++ setup/src/stork-websocket-client-for-oracle-data.mjs
@@ -0,0 +1,23 @@
+import WebSocket from 'ws';
+
+const ws = new WebSocket('wss://api-jp.stork.network/prices-v3', {
+    headers :{
+        Authorization: '12c149dd-2521-4441-b073-e9a2ade86b12'
+    }
+});
+
+//Example of subscribing to a single asset
+// Source :
+// https://docs.stork.network/hybrid-oracle-documentation
+
+
+ws.on('open', function open() {
+    ws.send('{"action":"subscribe","assets":["SUIUSD"]}');
+});
+
+ws.on('message', function message(data) {
+    console.log('received: %s', data);
+});
+
+
+ws.on('error', console.error);
