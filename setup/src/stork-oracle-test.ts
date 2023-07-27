abd18c451831e494ace070f566e3780b13ede9bd
Theodore Chaikalis <teo@mystenlabs.com>
2023-07-27 12:11:49 +0300

    minor

--- setup/src/stork-oracle-test.ts
+++ setup/src/stork-oracle-test.ts
@@ -5,12 +5,8 @@ import { getCoinsOfAddress } from "./examples/getCoinsOfAddress";
 console.log("Connecting to SUI network: ", SUI_NETWORK);
 
 const run = async () => {
-  const connection = new Connection({
-    fullnode: SUI_NETWORK,
-  });
-  const provider = new JsonRpcProvider(connection);
-  const coins = await getCoinsOfAddress({ provider, address: ADMIN_ADDRESS });
-  console.log(coins);
+  /// tbd. waiting for api key from stork
+
 };
 
 run();
